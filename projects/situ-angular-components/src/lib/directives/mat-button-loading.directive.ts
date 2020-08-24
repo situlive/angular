08912f8fb925229296085b0b41c3620460ab2c7a
Jaymie <jaymie@r3plica.co.uk>
2020-08-24 16:27:28 +0100

    Created the mat button loading directive

---
+++ projects/situ-angular-components/src/lib/directives/mat-button-loading.directive.ts
@@ -0,0 +1,81 @@
+import {
+  Directive,
+  OnChanges,
+  Input,
+  SimpleChanges,
+  ComponentFactory,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  Renderer2,
+  ComponentRef,
+} from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ThemePalette } from '@angular/material/core';
+
+@Directive({
+  selector: `button[mat-button][loading],
+             button[mat-raised-button][loading],
+             button[mat-icon-button][loading],
+             button[mat-fab][loading],
+             button[mat-mini-fab][loading],
+             button[mat-stroked-button][loading],
+             button[mat-flat-button][loading]`,
+})
+export class MatButtonLoadingDirective implements OnChanges {
+  @Input() loading: boolean;
+  @Input() disabled: boolean;
+  @Input() color: ThemePalette;
+
+  private spinnerFactory: ComponentFactory<MatProgressSpinner>;
+  private spinner: ComponentRef<MatProgressSpinner>;
+
+  constructor(
+    private matButton: MatButton,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private viewContainerRef: ViewContainerRef,
+    private renderer: Renderer2
+  ) {
+    this.spinnerFactory = this.componentFactoryResolver.resolveComponentFactory(
+      MatProgressSpinner
+    );
+  }
+
+  get nativeElement(): HTMLElement {
+    return this.matButton._elementRef.nativeElement;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes.loading) return;
+
+    if (changes.loading.currentValue) {
+      this.nativeElement.classList.add('mat-loading');
+      this.matButton.disabled = true;
+      this.createSpinner();
+    } else {
+      this.nativeElement.classList.remove('mat-loading');
+      this.matButton.disabled = this.disabled;
+      this.destroySpinner();
+    }
+  }
+
+  private createSpinner(): void {
+    if (!this.spinner) {
+      this.spinner = this.viewContainerRef.createComponent(this.spinnerFactory);
+      this.spinner.instance.color = this.color;
+      this.spinner.instance.diameter = 20;
+      this.spinner.instance.mode = 'indeterminate';
+      this.renderer.appendChild(
+        this.nativeElement,
+        this.spinner.instance._elementRef.nativeElement
+      );
+    }
+  }
+
+  private destroySpinner(): void {
+    if (this.spinner) {
+      this.spinner.destroy();
+      this.spinner = null;
+    }
+  }
+}
--- projects/situ-angular-components/src/lib/situ-angular-components.module.ts
+++ projects/situ-angular-components/src/lib/situ-angular-components.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonLoadingDirective } from './directives/mat-button-loading.directive';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatProgressBarModule],
+  declarations: [FooterComponent, HeaderComponent, MatButtonLoadingDirective],
+  exports: [FooterComponent, HeaderComponent, MatButtonLoadingDirective],
 })
 export class SituAngularComponentsModule {}
