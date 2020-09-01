c4be8798f8f7ad0e8e287258d8341b36355a661c
Jaymie <jaymie@r3plica.co.uk>
2020-09-01 18:46:18 +0100

    Updated the modules

--- projects/situ-angular-components/src/lib/situ-angular-components.module.ts
+++ projects/situ-angular-components/src/lib/situ-angular-components.module.ts
@@ -2,23 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MatButtonLoadingDirective } from './directives/mat-button-loading.directive';
-import { AnimationsModule } from './animations/animations.module';
-import { FooterModule } from './footer/footer.module';
-import { HeaderModule } from './header/header.module';
-import { LoaderModule } from './loader/loader.module';
-import { DirectivesModule } from './directives/directives.module';
-
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-
-    AnimationsModule,
-    DirectivesModule,
-    FooterModule,
-    HeaderModule,
-    LoaderModule,
-  ],
+  imports: [CommonModule, RouterModule],
 })
 export class SituAngularComponentsModule {}
