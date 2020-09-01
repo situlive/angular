ceb04324dbd62779c37b36d22a99a809dbef4f98
Jaymie <jaymie@r3plica.co.uk>
2020-09-01 18:37:41 +0100

    Separated each component with their own modules

--- projects/situ-angular-components/src/lib/situ-angular-components.module.ts
+++ projects/situ-angular-components/src/lib/situ-angular-components.module.ts
@@ -2,38 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { LoaderComponent } from './loader/loader.component';
-
-import { MatButtonLoadingDirective } from './mat-button-loading.directive';
-import { AnimateDirective } from './animate.directive';
+import { MatButtonLoadingDirective } from './directives/mat-button-loading.directive';
+import { AnimationsModule } from './animations/animations.module';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
+import { LoaderModule } from './loader/loader.module';
+import { DirectivesModule } from './directives/directives.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-  ],
-  declarations: [
-    FooterComponent,
-    HeaderComponent,
-    LoaderComponent,
 
-    AnimateDirective,
-    MatButtonLoadingDirective,
-  ],
-  exports: [
-    FooterComponent,
-    HeaderComponent,
-    LoaderComponent,
-
-    AnimateDirective,
-    MatButtonLoadingDirective,
+    AnimationsModule,
+    DirectivesModule,
+    FooterModule,
+    HeaderModule,
+    LoaderModule,
   ],
 })
 export class SituAngularComponentsModule {}
--- projects/situ-angular-components/src/public-api.ts
+++ projects/situ-angular-components/src/public-api.ts
@@ -2,13 +2,11 @@
  * Public API Surface of situ-angular-components
  */
 
-export * from './lib/footer/footer.component';
-export * from './lib/header/header.component';
-export * from './lib/loader/loader.component';
-
-export * from './lib/animate.directive';
-export * from './lib/mat-button-loading.directive';
-export * from './lib/situ-angular-components.module';
+export * from './lib/animations/animations.module';
+export * from './lib/directives/directives.module';
+export * from './lib/footer/footer.module';
+export * from './lib/header/header.module';
+export * from './lib/loader/loader.module';
 
 export * from './lib/animations/collapse';
 export * from './lib/animations/fade';
