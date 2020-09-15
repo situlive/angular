cf3e304177ed02549ae25aa0fdbdf021e9fbb7af
Jaymie <jaymie@r3plica.co.uk>
2020-09-15 09:42:01 +0100

    Added the router module to the header and footer components

--- projects/situ-angular-components/src/lib/directives/directives.module.ts
+++ projects/situ-angular-components/src/lib/directives/directives.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { MatButtonLoadingDirective } from './mat-button-loading.directive';
 
 @NgModule({
   declarations: [MatButtonLoadingDirective],
   exports: [MatButtonLoadingDirective],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatProgressSpinnerModule],
 })
 export class DirectivesModule {}
--- projects/situ-angular-components/src/lib/footer/footer.module.ts
+++ projects/situ-angular-components/src/lib/footer/footer.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
   exports: [FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class FooterModule {}
