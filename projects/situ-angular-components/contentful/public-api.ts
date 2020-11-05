340f62354f958d76e58e99eb4d4804181afd903d
Jaymie <jaymie@r3plica.co.uk>
2020-11-05 17:17:50 +0000

    Created the contentful component

--- projects/situ-angular-components/contentful/public-api.ts
+++ projects/situ-angular-components/contentful/public-api.ts
@@ -6,6 +6,7 @@ export * from './_core/services/index';
 
 /* These are exported so they can be referenced in code behind */
 export * from './footer/footer.component';
+export * from './contentful/contentful.component';
 export * from './contentful-page/contentful-page.component';
 export * from './universal-contentful-page/universal-contentful-page.component';
 
