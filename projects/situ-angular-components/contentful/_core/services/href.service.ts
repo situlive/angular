d5a2e32ee02acb755ceca9782d33c7f8ae2bb81b
Jaymie <jaymie@r3plica.co.uk>
2020-12-21 13:36:42 +0000

    Added getQueryParams and getHref to the hrefService

--- projects/situ-angular-components/contentful/_core/services/href.service.ts
+++ projects/situ-angular-components/contentful/_core/services/href.service.ts
@@ -25,4 +25,26 @@ export class HrefService {
     const links = url.split('#');
     return links.length === 2 ? links[1] : '';
   }
+
+  public getQueryParams(url: string): string {
+    let routes = url.split('?');
+    let query = routes.length > 1 ? routes[1] : '';
+    let params = query ? this.getParams(query) : undefined;
+    return params;
+  }
+
+  public getHref(url: string): string {
+    let routes = url.split('?');
+    let routeWithoutQuery = routes[0];
+
+    let fragments = routeWithoutQuery.split('#');
+    return fragments[0];
+  }
+
+  private getParams(query: string): any {
+    let params = query.split('=');
+    let key = params[0];
+    let value = params.length > 1 ? params[1] : '';
+    return JSON.parse(`{ "${key}": "${value}" }`);
+  }
 }
