6bdea19cb2d990215f4968a21b193dd1d87a4a21
Jaymie <jaymie@r3plica.co.uk>
2021-01-07 18:33:11 +0000

    Added brand, plan and venue to the subscription response

--- projects/situ-angular-components/data/_core/models/subscription.ts
+++ projects/situ-angular-components/data/_core/models/subscription.ts
@@ -1,3 +1,7 @@
+import { Brand } from './brand';
+import { Plan } from './plan';
+import { Venue } from './venue';
+
 export class Subscription {
   id: number;
   brandId: number;
@@ -11,4 +15,25 @@ export class Subscription {
   productCount: number;
   price: number;
   termsAgreed: boolean;
+
+  brand?: Brand;
+  plan?: Plan;
+  venue?: Venue;
+
+  public stateName(): string {
+    switch (this.state) {
+      case 0:
+        return 'Reserved';
+      case 1:
+        return 'Pending';
+      case 2:
+        return 'Active';
+      case 3:
+        return 'Cancelled';
+      case 4:
+        return 'Expired';
+      default:
+        return 'Unknown';
+    }
+  }
 }
