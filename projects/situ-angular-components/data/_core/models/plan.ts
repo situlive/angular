da590ccbfff0bf158e5ea05d0cdc5f1c3ac84fb0
Jaymie <jaymie@r3plica.co.uk>
2021-01-07 16:14:46 +0000

    Added price and productCount to plans

--- projects/situ-angular-components/data/_core/models/plan.ts
+++ projects/situ-angular-components/data/_core/models/plan.ts
@@ -1,12 +1,14 @@
 import { BaseModel } from './base-model';
 
 export class Plan implements BaseModel {
-    id: number;
-    name: string;
-    description?: string;
-    quantity?: number;
-    booked?: number;
-    available?: number;
+  id: number;
+  price: number;
+  productCount: number;
+  name: string;
+  description?: string;
+  quantity?: number;
+  booked?: number;
+  available?: number;
 
-    theatres?: any[];
+  theatres?: any[];
 }
