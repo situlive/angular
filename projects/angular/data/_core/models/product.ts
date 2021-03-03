46206671d074f31f41f7359b5bea57ab5b95445c
Jaymie <jaymie@r3plica.co.uk>
2021-03-03 09:54:18 +0000

    Updated the product model

--- projects/angular/data/_core/models/product.ts
+++ projects/angular/data/_core/models/product.ts
@@ -5,10 +5,35 @@ import { IKey } from './key';
 export class Product extends Base implements IKey {
   id: string;
   variant: string;
+  categoryId: string;
+
   title: string;
-  categoryId: number;
+  brand: string;
+  colour: string;
+  summary?: string;
+  description?: string;
+  imageUrl?: string;
+  modelGroup?: string;
+  modelAlias?: string;
+  modelSubGroup?: string;
+
+  scenarios?: string[];
+
   state: 'Complete' | 'Incomplete' | 'Live' | 'NewEntry';
-  specification: SpecificationData;
-  images: string[];
+
+  hasSpecification?: boolean;
+  specification?: SpecificationData;
+  specificationFeedId?: number;
+
+  hasContextualData?: boolean;
+  contextData?: any;
+  contextualizationCreated?: string;
+  contextualizationUpdated?: string;
+
+  images?: string[];
+  retailers?: any[];
+  retailersUpdated?: string;
+  priceDropPercentage?: number;
+
   selected?: boolean;
 }
